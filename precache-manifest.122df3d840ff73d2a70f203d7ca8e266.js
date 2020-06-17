8da7fef2267b7b412dc09582518ccfb516db80e8
baciey <baciey@gmail.com>
2020-06-17 13:46:17 +0200

    Updates

---
+++ precache-manifest.122df3d840ff73d2a70f203d7ca8e266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441ace62050730e1e724b9ebc771a798",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "66a052a2b2a36a351ac7",
+    "url": "/hangman/static/css/main.ef64e806.chunk.css"
+  },
+  {
+    "revision": "b5bc4a72133d3879d1d7",
+    "url": "/hangman/static/js/2.eb761805.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.eb761805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a052a2b2a36a351ac7",
+    "url": "/hangman/static/js/main.b87dbb64.chunk.js"
+  },
+  {
+    "revision": "55f49f1123807bea70cd",
+    "url": "/hangman/static/js/runtime-main.8a7fa963.js"
+  }
+]);
